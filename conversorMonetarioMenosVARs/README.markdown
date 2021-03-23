7ff42377facaa1023d7b9f2b5fe9555e2f7203a5
David Teixeira de Masin <81026193+davidtmasin@users.noreply.github.com>
2021-03-23 12:30:27 -0300

    Remoção de duplicata

--- conversorMonetarioMenosVARs/README.markdown
+++
@@ -1,5 +0,0 @@
-# Conversor Monetário Menos VARs
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/davidtmasin/pen/vYgEmOL](https://codepen.io/davidtmasin/pen/vYgEmOL).
-
-
--- conversorMonetarioMenosVARs/dist/script.js
+++
@@ -1,7 +0,0 @@
-var valorDolar = parseFloat(prompt("Informe o valor do produto em U$ que você deseja converter, exemplo: 299.99"))
-
-// Matemática da conversão do valor do dólar U$ em R$
-var valorBRL = (valorDolar * 5.51).toFixed(2)
-
-
-alert("U$" + valorDolar + " equivale a " + "R$" + valorBRL)
