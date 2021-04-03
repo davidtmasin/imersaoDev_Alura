6c791e49d8e9b1764135bbf5f9981de8f296a429
David Teixeira de Masin <81026193+davidtmasin@users.noreply.github.com>
2021-04-03 10:56:41 -0300

    Desafio - Aula 06
    botão empate já atribui os pontos para todos os jogadores.

---
+++ Aula 06/desafiosImersaoDev/tabela-de-classifica-odesafio-da-rafa/README.markdown
@@ -0,0 +1,5 @@
+# Tabela de classificação - Desafio da Rafa
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/davidtmasin/pen/NWdjbpa](https://codepen.io/davidtmasin/pen/NWdjbpa).
+
+
---
+++ Aula 06/desafiosImersaoDev/tabela-de-classifica-odesafio-da-rafa/src/script.js
@@ -0,0 +1,70 @@
+// Varíavel objeto
+var paulo = {
+  nome: "Paulo",
+  vitorias: 2,
+  empates: 5,
+  derrotas: 1,
+  pontos: 0
+
+var rafa = {
+  nome: "Rafa",
+  vitorias: 3,
+  empates: 5,
+  derrotas: 2,
+  pontos: 0
+}
+
+rafa.pontos = calculaPontos(rafa)
+paulo.pontos = calculaPontos(paulo)
+
+
+function calculaPontos(jogador){
+  
+  var pontos = (jogador.vitorias * 3) + (jogador.empates * 1) + (jogador.derrotas * 0)
+
+  return pontos
+  
+}
+
+var jogadores = [rafa, paulo]
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores){
+  var html = ""
+  for(var i = 0; i < jogadores.length; i++){
+    html += "<tr><td>" + jogadores[i].nome + "</td>"
+    html += "<td>" + jogadores[i].vitorias + "</td>"
+    html += "<td>" + jogadores[i].empates + "</td>"
+    html += "<td>" + jogadores[i].derrotas + "</td>"
+    html += "<td>" + jogadores[i].pontos + "</td>"
+    html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+    html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+    html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>"
+  }
+  var tabelaJogadores = document.getElementById("tabelaJogadores")
+  tabelaJogadores.innerHTML = html
+}
+
+function adicionarVitoria(i){
+  var jogador = jogadores[i]
+  jogador.vitorias++
+  jogador.pontos = calculaPontos(jogador)
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i){
+  var jogador1 = jogadores[0]
+  var jogador2 = jogadores[1]
+  jogador1.empates++
+  jogador2.empates++
+  jogador1.pontos = calculaPontos(jogador1)
+  jogador2.pontos = calculaPontos(jogador2)
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i){
+  // console.log("clicou no botão Derrota")
+  var jogador = jogadores[i]
+  jogador.derrotas++
+  exibirJogadoresNaTela(jogadores)
+}
